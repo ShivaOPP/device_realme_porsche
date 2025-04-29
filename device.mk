d0f5b6fe89d6cc43ced7fdbf2e0fed75671f3789
Bruno Martins <bgcngm@gmail.com>
2025-04-30 00:28:16 +0100

    porsche: Set Lineage Health control charging path

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2023 The LineageOS Project
+# Copyright (C) 2021-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -40,6 +40,9 @@ PRODUCT_SYSTEM_PROPERTIES += \
 # Display
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.brightness.disable_gamma_conversion=true
+
+# Lineage Health
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
 
 # NFC
 PRODUCT_PACKAGES += \
